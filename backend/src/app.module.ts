1c6fc6012e1067473b462f9201f5fc07ef16a3ce
mizamarzes <mizamarzes@gmail.com>
2025-12-18 10:20:33 -0500

    feat: implement user and auth modules with PostgreSQL integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule, 
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'your_username',
+      password: 'your_password',
+      database: 'your_database',
+      entities: [User],
+    })
+  ],
 })
 export class AppModule {}
---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: 'varchar', length: 100, unique: true})
+    email: string;
+
+    @Column({type: 'varchar', length: 100})
+    password: string;
+
+    @Column({type: 'varchar', length: 50})
+    name: string;
+
+    @Column({type: 'boolean', default: true})
+    isActive: boolean;
+}
