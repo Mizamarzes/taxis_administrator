539afb315f7a3c0c3600930d5805569b3c9955cb
mizamarzes <mizamarzes@gmail.com>
2026-01-30 16:01:42 -0500

    feat: add Page and DashboardCard components; integrate into App layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { AppSideBar } from "./components/AppSideBar";
 import { Header } from "./components/Header";
+import { Page, PageHeader } from "./components/Page";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { SidebarInset, SidebarProvider } from "./components/ui/sidebar";
 // import AppRouter from "./routes/AppRouter";
@@ -13,7 +14,15 @@ const App = () => {
                 <Header />
 
                 <main>
-                    
+                    <Page>
+                        <PageHeader />
+
+                        <div className="">
+                            <DashBoardCard>
+                                
+                            </DashBoardCard>
+                        </div>
+                    </Page>
                 </main>
             </SidebarInset>
         </SidebarProvider>
---
+++ frontend/src/components/DashboardCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter} from './ui/card'
+
+export const DashboardCard = () => {
+  return (
+    <div>DashboardCard</div>
+  )
+}
