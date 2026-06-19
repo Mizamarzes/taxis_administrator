4f4fd6d5e27bd6d5b4a888335e56ee10ba5ecc88
mizamarzes <mizamarzes@gmail.com>
2026-06-19 08:59:38 -0500

    fix: :bug: a spanish

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -2,11 +2,11 @@ import { Transform } from 'class-transformer';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
-    @IsEmail()
+    @IsEmail({}, { message: 'El correo electrónico no es válido' })
     email: string;
 
     @Transform(({ value }) => value.trim())
-    @IsString()
-    @MinLength(6)
+    @IsString({ message: 'La contraseña debe ser un texto' })
+    @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
     password: string;
 }
